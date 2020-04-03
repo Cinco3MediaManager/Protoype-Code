f6ae012ff06f1c601ef0a00f7a57273baf3e978c
Cinco3MediaManager <62650218+Cinco3MediaManager@users.noreply.github.com>
2020-04-03 00:21:54 -0400

    Update CincoTresProtocol.java

--- CincoTresProtocol.java
+++ CincoTresProtocol.java
@@ -1,42 +1,48 @@
 import java.net.*;
 import java.io.*;
 
+//For testing Purposes Only. Class will need to be written/rewritten to behave properly. This class processes server input and 
+//determines action to take. Also formats output and retusn to server for sending to client
 public class CincoTresProtocol 
 {
 	int type = 0;
-
-    public String processInput(String theInput)
-    {
-    	String theOutput = "";
-    	int len = theInput.length();
+	
+	//Testing only. processInput is passed input from the server and processes it. A cooresponding "processOutput" will need to 
+   	//Be created
+	public String processInput(String theInput)
+	{
+    		String theOutput = "";
+    		int len = theInput.length();
     	
-    	if(len > 0)
-    	{
+    		if(len > 0)
+    		{
    			char command = theInput.charAt(0);
    			
    			if(command == '&')		// & is the prefix for client-server first exchange;
    			{
    				theOutput = "Welcome to C3 Media Manager. This is the output console for the server.";
-    		}
+    			}
     	
-    		if(command == '*')		// * is the prefix for username and password pairs; seperated by #. Ex: *username#password
-    		{    		
+    			if(command == '*')		// * is the prefix for username and password pairs; seperated by #. Ex: *username#password
+    			{	
+				//Testing only. replace with method for verifying credentials. ex: verifyCredentials(some string)
    				if(theInput.equals("*jeb#pass"))
    				{
    					theOutput = "verified";
    				}	
    				else
-    			{
-    				theOutput = "Invalid Password or Username";
-    			}
+    				{
+    					theOutput = "Invalid Password or Username";
+    				}
    			}
     		
-    	}
+    		}
     	
-    	else
-    	{
+    		else
+    		{	
    			theOutput = "";
-    	}
+    		}
+		
     	return theOutput; 	
     }
 }
